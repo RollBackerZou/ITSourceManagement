43631dc68da83898b498047f938cd2844291f542
Zou <zdc930818@163.com>
2018-03-12 16:33:20 +0800

    添加route

--- ITSourceManagement/App_Start/RouteConfig.cs
+++ ITSourceManagement/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace ITSourceManagement
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Default1",
+                url: "{Area}/{controller}/{action}/{id}",
+                defaults: new { Area="BackEnd",controller="Home",action="Index",id = UrlParameter.Optional}
+                );
         }
     }
 }
